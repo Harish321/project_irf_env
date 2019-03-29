9b4dc53a524a821df139756bc6386cc1fe3d5ed8
Harish Dhulipalla <Harish@administrators-MacBook-Air.local>
2019-03-29 17:40:53 +0530

    success style hsa been added

--- extension/perdix-env/script.js
+++ extension/perdix-env/script.js
@@ -3,17 +3,32 @@ var setEnv = function(event){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        console.log(this.responseText);
+        var response = JSON.parse(this.responseText);
+        if (response.status == "success"){
+            chrome.storage.local.set({'currentEnv':env},function(){});
+            event.target.classList.add('btn-success');
+        }
     }
 };
 xhttp.open("GET", "http://localhost:3000/api/setenv?data="+env, true);
 xhttp.send();
 }
 document.addEventListener('DOMContentLoaded', function() {
-    var classname = document.getElementsByClassName('env_button');
+    var currentEnv;
+    chrome.storage.local.get(['currentEnv'],function(value){
+        currentEnv = value.currentEnv ? value.currentEnv : null;
+        var classname = document.getElementsByClassName('env_button');
     Array.from(classname).forEach(function(element) {
+        if (element.value == currentEnv)
+            element.classList.add('btn-success');
         element.addEventListener('click', function(event){
+            var classname = document.getElementsByClassName('env_button');
+            Array.from(classname).forEach(function(element) {
+                element.classList.remove('btn-success');
+            })
             setEnv(event);
         });
       })
+    })
+    
 });
